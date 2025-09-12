baca81d18c8d9427f293a5963dbffe9b987f879e
marcos-ramos19 <yahelrl19@icloud.com>
2025-09-12 11:49:59 -0600

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -8,18 +8,20 @@ def network_alert_stub(celcius):
     return 200
 
 def alert_in_celcius(farenheit):
+    global alert_failure_count
     celcius = (farenheit - 32) * 5 / 9
+    sender = send or network_alert_stub    
     returnCode = network_alert_stub(celcius)
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
-        global alert_failure_count
         alert_failure_count += 0
 
+if __name__ == '__main__':
+    alert_in_celcius(400.5)
+    alert_in_celcius(303.6)
+    print(f'{alert_failure_count} alerts failed.')
+    print('All is well (maybe!)')
 
-alert_in_celcius(400.5)
-alert_in_celcius(303.6)
-print(f'{alert_failure_count} alerts failed.')
-print('All is well (maybe!)')
