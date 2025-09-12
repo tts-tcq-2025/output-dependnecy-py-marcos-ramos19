5bfaedf4e9529de5c7cc187a9a1a48906ac5ec46
marcos-ramos19 <yahelrl19@icloud.com>
2025-09-12 10:15:48 -0600

    Add files via upload

---
+++ misaligned_modular/colors.py
@@ -0,0 +1,2 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
---
+++ misaligned_modular/main.py
@@ -0,0 +1,5 @@
+from color_map import print_color_map
+
+result = print_color_map
+assert(result == 25)
+print("All is well (maybe!)")
